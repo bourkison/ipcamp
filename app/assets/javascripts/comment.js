8e3edd9d1293061929527f68886da75774942f0e
Harry <harrisonbourke97@gmail.com>
2017-09-21 01:44:32 -0400

    fuck you heroku number 2

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,17 +1,19 @@
-$(document).ready(function() {
-  setInterval(function() {
-    let url = location.pathname.split('/')
+'use strict';
+
+$(document).ready(function () {
+  setInterval(function () {
+    var url = location.pathname.split('/');
     if (url[1] === "listings") {
-      $.get('/comments_for/' + url[2]).done(function(data) {
+      $.get('/comments_for/' + url[2]).done(function (data) {
         data = JSON.parse(data.comments);
         console.log(data);
-        let s = "";
+        var s = "";
 
-        for (let i = 0; i < data.length; i++) {
-          s += `<li>${data[i].content} | <a href='/users/${data[i].user.id}'>${data[i].user.username}</a></li>`
+        for (var i = 0; i < data.length; i++) {
+          s += '<li>' + data[i].content + ' | <a href=\'/users/' + data[i].user.id + '\'>' + data[i].user.username + '</a></li>';
         }
         $('#comments').html(s);
-      })
+      });
     }
-  }, 1000)
-})
+  }, 1000);
+});
